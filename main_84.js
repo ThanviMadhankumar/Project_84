4f0228c15c1778860c6ce9d55658ee3bb92f5387
Thanvi <102605577+ThanviMadhankumar@users.noreply.github.com>
2022-07-05 11:23:39 +0530

    Add files via upload

--- main_84.js
+++ main_84.js
@@ -6,7 +6,7 @@ function getmenu()
         var item = document.getElementById("pizza_name").value; 
        
         //Add push() function to push item into array
-        pizza_list_array. push()(item);
+        pizza_list_array.push(item);
         
         var remove_commas = pizza_list_array.join("<br># ");
         document.getElementById("display_menu").innerHTML =  "# " + remove_commas;
